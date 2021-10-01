bdb3ea3606489a2b4c58cbeec88c7d564f65bc6e
Nathan Brooks <natebrooks004@gmail.com>
2021-09-30 23:28:58 -0700

    main

---
+++ main.ts
@@ -0,0 +1,52 @@
+import { filter } from "./filter.ts";
+import { map } from "./map.ts";
+import { range } from "./range.ts";
+import { take } from "./take.ts";
+import { toArray } from "./to_array.ts";
+
+/** Patch Generator typing */
+declare global {
+  interface Generator {
+    map: typeof map;
+    filter: typeof filter;
+    take: typeof take;
+    toArray: typeof toArray;
+  }
+}
+
+/** Callback function type used for some iterator helpers. */
+export interface CallbackFn<T, U> {
+  (value: T, index: number): U;
+}
+
+/** Array of helpers added to `Generator`. */
+export const iteratorHelpers = [
+  map,
+  filter,
+  take,
+  toArray,
+];
+
+/** Generator function prototype. */
+export const GeneratorFunction = Object.getPrototypeOf(range).prototype;
+
+main(); // apply monkey-patch
+
+/** Applies iterator helper monkey-patch. */
+function main() {
+  const propertyDescriptors = iteratorHelpers.map(iteratorHelperToProperty);
+  const propertyDescriptorMap = Object.fromEntries(propertyDescriptors);
+  Object.defineProperties(GeneratorFunction, propertyDescriptorMap);
+}
+
+/** Returns an `Array` of name and object of property descriptors. */
+export function iteratorHelperToProperty(
+  helper: typeof iteratorHelpers[number],
+) {
+  return [helper.name, {
+    writable: true,
+    enumerable: false,
+    configurable: true,
+    value: helper,
+  }];
+}
