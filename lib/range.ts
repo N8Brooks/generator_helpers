179b7edb494895a8d782c07b6b22458128f2ee15
Nathan Brooks <natebrooks004@gmail.com>
2021-10-03 15:37:03 -0700

    three parameter range

--- lib/range.ts
+++ lib/range.ts
@@ -1,9 +1,30 @@
 /** Yields `stop` `numbers` from `0` to `stop - 1`. */
-export function* range(stop: number): Generator<number> {
-  if (!Number.isInteger(stop)) {
-    throw RangeError("stop must be an integer");
+export function* range(y0: number, y1?: number, yd = 1): Generator<number> {
+  if (!Number.isFinite(y0)) {
+    throw RangeError("range() y0 must be a finite number");
   }
-  for (let i = 0; i < stop; i++) {
-    yield i;
+
+  if (y1 === undefined) {
+    // y0 is treated as the stop, yd is ignored.
+    for (let n = 0; n < y0; n++) {
+      yield n;
+    }
+    return;
+  } else if (!Number.isFinite(y1)) {
+    throw RangeError("range() y1 must be a finite number");
+  }
+
+  if (yd === 0 || !Number.isFinite(yd)) {
+    throw RangeError("range() yd argument must be a non-zero finite number");
+  } else if (yd < 0) {
+    // increment
+    for (let n = y0; n > y1; n += yd) {
+      yield n;
+    }
+  } else {
+    // decrement
+    for (let n = y0; n < y1; n += yd) {
+      yield n;
+    }
   }
 }
--- lib/range_test.ts
+++ lib/range_test.ts
@@ -1,29 +1,102 @@
+import "./main.ts";
 import {
   assertEquals,
   assertThrows,
 } from "https://deno.land/std@0.108.0/testing/asserts.ts";
 import { range } from "./range.ts";
 
-for (const n of [NaN, -Infinity, 0.5, 1 / 7, Math.PI, Infinity]) {
-  Deno.test(n.toString(), () => {
+for (const n of [NaN, Infinity, -Infinity]) {
+  Deno.test(`range(${n})`, () => {
     assertThrows(
       () => [...range(n)],
       RangeError,
-      "stop must be an integer",
+      "range() y0 must be a finite number",
+    );
+  });
+
+  Deno.test(`range(0, ${n})`, () => {
+    assertThrows(
+      () => [...range(0, n)],
+      RangeError,
+      "range() y1 must be a finite number",
+    );
+  });
+
+  Deno.test(`range(0, 10, ${n})`, () => {
+    assertThrows(
+      () => [...range(0, 10, n)],
+      RangeError,
+      "range() yd argument must be a non-zero finite number",
     );
   });
 }
 
-for (const n of [-Number.MAX_SAFE_INTEGER, -10, -1, 0]) {
-  Deno.test(n.toString(), () => {
+for (const n of [-Number.MIN_VALUE, -1, 0]) {
+  Deno.test(`range(${n})`, () => {
     assertEquals([...range(n)], []);
   });
 }
 
-for (const n of [1, 2, 5, 10, 100]) {
-  Deno.test(n.toString(), () => {
+for (const n of [1, 0.5, Math.PI, 10, 100]) {
+  Deno.test(`range(${n})`, () => {
     const actual = [...range(n)];
-    const expected = Array(n).fill(null).map((_, i) => i);
+    const expected = Array(Math.ceil(n)).fill(0).map((_, i) => i);
     assertEquals(actual, expected);
   });
+}
+
+Deno.test(`range(0, 11, 0)`, () => {
+  assertThrows(
+    () => [...range(0, 10, 0)],
+    RangeError,
+    "range() yd argument must be a non-zero finite number",
+  );
+});
+
+for (const y0 of [-5, 0, 1, 0.5, 10]) {
+  for (const y1 of [-5, 0, 1, 0.5, 10]) {
+    Deno.test(`range(${y0}, ${y1})`, () => {
+      const actual = [...range(y0, y1)];
+      const expected: number[] = [];
+      let n = y0;
+      while (n < y1) {
+        expected.push(n++);
+      }
+      assertEquals(actual, expected);
+    });
+  }
+}
+
+for (const y0 of [-5, 0, 1, 0.5, 10]) {
+  for (const y1 of [-5, 0, 1, 0.5, 10]) {
+    for (const yd of [1, 0.5, 10]) {
+      Deno.test(`range(${y0}, ${y1}, ${yd})`, () => {
+        const actual = [...range(y0, y1, yd)];
+        const expected: number[] = [];
+        let n = y0;
+        while (n < y1) {
+          expected.push(n);
+          n += yd;
+        }
+        assertEquals(actual, expected);
+      });
+    }
+  }
+}
+
+for (const y0 of [-5, 0, 1, 0.5, 10]) {
+  for (const y1 of [-5, 0, 1, 0.5, 10]) {
+    for (const yd of [-1, -3.7]) {
+      Deno.test(`range(${y0}, ${y1}, ${yd})`, () => {
+        const actual = [...range(y0, y1, yd)];
+        const expected: number[] = [];
+        let n = y0;
+        while (n > y1) {
+          expected.push(n);
+          n += yd;
+        }
+        assertEquals(actual, expected);
+      });
+    }
+  }
 }
